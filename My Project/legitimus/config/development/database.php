5e87922f56b3361cf2879c29f05a22b36106a77a
e.tolubarya <e.tolubarya@908.dp.ua>
2014-12-14 23:51:51 +0200

    Update database.php

--- My Project/legitimus/config/development/database.php
+++ My Project/legitimus/config/development/database.php
@@ -1,10 +1,20 @@
 <?php
 
+/**
+ * Config for databases access
+ */
 return [
     'mysql' => [
         'username' => 'root',
-        'username' => '',
+        'password' => '',
         'host' => 'localhost',
         'db' => 'legitimus',
-    ]
+    ],
+    'mongo' => [
+        'username' => 'user',
+        'password' => '',
+        'host' => 'localhost',
+        'port' => '27017',
+        'db' => 'legitimus',
+    ],
 ];
