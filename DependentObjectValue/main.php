bbf4a6688cad4159947ab8f876797f1593c138ea
e.tolubarya <e.tolubarya@908.dp.ua>
2014-12-17 01:01:43 +0200

    Update main.php

--- DependentObjectValue/main.php
+++ DependentObjectValue/main.php
@@ -7,8 +7,8 @@ $y = new DependentObjectValue($x, function($value) {
     return pow($value, 2) + 2 * $value;
 });
 
-echo "[ x ; y ]\n";
+echo "[ x; y]\n";
 foreach (range(-5, 5) as $value) {
     $x->set($value);
-    echo "[$x;$y]\n";
+    echo "[$x; $y]\n";
 }
