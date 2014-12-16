3fd0c78f0168f51ac895c97d9521a98dc011719d
e.tolubarya <e.tolubarya@908.dp.ua>
2014-12-17 01:04:33 +0200

    Created by Coding Ground Client

---
+++ Legitimus/legitimus/App/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @namespace
+ */
+namespace App\Controller;
+
+use Fw\Mvc\Controller;
+
+/**
+ * Class UserController
+ */
+class UserController extends Controller
+{
+    /**
+     * Default action
+     * 
+     * @return void
+     */
+    public function indexAction()
+    {
+    }
+}
