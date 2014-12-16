4fc6d5eb1745e16d68e00579e6599c1a7d57719f
e.tolubarya <e.tolubarya@908.dp.ua>
2014-12-17 00:52:58 +0200

    Created by Coding Ground Client

---
+++ DependentObjectValue/main.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'DependentObjectValue.php';
+
+$x = new DependentObjectValue();
+$y = new DependentObjectValue($x, function($value) {
+    return pow($value, 2) + 2 * $value;
+});
+
+echo "[ x ; y ]\n";
+foreach (range(-5, 5) as $value) {
+    $x->set($value);
+    echo "[$x;$y]\n";
+}
