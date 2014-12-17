44fb68de963b8780142e4cbb189babebd3a302ac
e.tolubarya <e.tolubarya@908.dp.ua>
2014-12-17 23:56:56 +0200

    Update index.php

--- Legitimus/legitimus/public/index.php
+++ Legitimus/legitimus/public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 require_once __DIR__ . '/../bootstrap.php';
 
 $config = require APPLICATION_PATH . 'config/' . APP_ENVIRONMENT . '/main.php';
