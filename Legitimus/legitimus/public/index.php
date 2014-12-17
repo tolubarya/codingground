de9b2bb4cb48d6beda65b1bc3497c80d00fe7f19
e.tolubarya <e.tolubarya@908.dp.ua>
2014-12-18 00:08:09 +0200

    Update index.php

--- Legitimus/legitimus/public/index.php
+++ Legitimus/legitimus/public/index.php
@@ -7,23 +7,18 @@ require_once __DIR__ . '/../bootstrap.php';
 
 $config = require APPLICATION_PATH . 'config/' . APP_ENVIRONMENT . '/main.php';
 
-/** @TODO Config must be implement the pattern Registry */
-/** @TODO Need to implement the Fw\Application */
-/** @TODO Need to implement the Fw\Http\Request */
-/** @TODO Need to implement the Fw\Http\Response */
-/** @TODO Need to implement the Fw\Router */
-/** @TODO Need to implement the Fw\Dispatcher */
-
-/** @TODO This code must be moved to the controller */
 try {
-    $userId = 1;
-    $user = App\Model\User::findByPk($userId);
-    
-    if ($user) {
-        echo json_encode($user) . PHP_EOL;
-    } else {
-        echo 'User not found' . PHP_EOL;
-    }
+    /** @TODO Config must be implement the pattern Registry */
+    /** @TODO Need to implement the Fw\Application */
+    /** @TODO Need to implement the Fw\Http\Request */
+    /** @TODO Need to implement the Fw\Http\Response */
+    /** @TODO Need to implement the Fw\Router */
+    /** @TODO Need to implement the Fw\Dispatcher */
+    /** @TODO Need to implement the Fw\DependencyInjector */
+
+    /** Test controller */
+    $controller = new App\Controller\UserController();
+    $controller->index();
 } catch(Exception $e) {
     echo $e . PHP_EOL;
 }
