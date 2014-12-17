955804619d25da1f9bc70d648c68a66787cd3516
e.tolubarya <e.tolubarya@908.dp.ua>
2014-12-17 02:02:02 +0200

    Update main.php

--- DependentObjectValue/main.php
+++ DependentObjectValue/main.php
@@ -1,9 +1,22 @@
 <?php
 
+/** @TODO Implement a class of dependent objects on events */
 require_once 'DependentObjectValue.php';
+
+############### Example 1 ###############
+
+$x = new DependentObjectValue(10);
+$y = new DependentObjectValue($x, function($value) { return $value + 5; });
+
+echo "y = $y\n";
+$x->set(20);
+echo "y = $y\n";
+
+############### Example 2 ###############
 
 $x = new DependentObjectValue();
 $y = new DependentObjectValue($x, function($value) {
+    /** Calculation of the parabola */
     return pow($value, 2) + 2 * $value;
 });
 
