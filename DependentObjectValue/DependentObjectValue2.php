74e9b621b11ea4094d3b2ff619e5376741609406
e.tolubarya <e.tolubarya@908.dp.ua>
2014-12-21 01:04:03 +0200

    Created by Coding Ground Client

---
+++ DependentObjectValue/DependentObjectValue2.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once 'DependentObjectValueInterface.php';
+require_once 'EventsManager.php';
+
+class DependentObjectValue2 implements DependentObjectValueInterface
+{
+    const EVENT_PREFIX = 'DOVEvent';
+
+    private $value;
+    private $callback;
+    private $event;
+
+    private static $number = 0;
+
+    public function __construct($value = null, Closure $callback = null)
+    {
+        $this->event = static::EVENT_PREFIX . self::$number++;
+        $this->set($value, $callback);
+    }
+
+    public function __toString()
+    {
+        return (string) $this->get();
+    }
+
+    public function set($value = null, Closure $callback = null)
+    {
+        if ($callback) {
+            $this->callback = $callback;
+        }
+
+        if ($value instanceof self) {
+            EventsManager::on($value->getEvent(), [$this, 'set']);
+        } else {
+            if ($this->callback) {
+                $value = $this->callback->__invoke($value);
+            }
+            $this->value = $value;
+            EventsManager::fire($this->getEvent(), $this->value);
+        }
+    }
+
+    public function get()
+    {
+        return $this->value;
+    }
+
+    public function getEvent()
+    {
+        return $this->event;
+    }
+}
