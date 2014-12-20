8bddb12f3a39dba5c9f8179382fa543260a1ff85
e.tolubarya <e.tolubarya@908.dp.ua>
2014-12-21 01:27:42 +0200

    Update EventsManagerInterface.php

--- DependentObjectValue/EventsManagerInterface.php
+++ DependentObjectValue/EventsManagerInterface.php
@@ -2,7 +2,7 @@
 
 interface EventsManagerInterface
 {
-    public static function fire($event, $args);
-    public static function on($event, $handler);
-    public static function off($event, $handler);
+    public function fire($event, $args);
+    public function on($event, $handler);
+    public function off($event, $handler);
 }
