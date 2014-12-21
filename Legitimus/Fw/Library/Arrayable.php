89a1120c6c3c3882ecd9321b9c8736a96929c561
e.tolubarya <e.tolubarya@908.dp.ua>
2014-12-21 11:42:30 +0200

    Update Arrayable.php

--- Legitimus/Fw/Library/Arrayable.php
+++ Legitimus/Fw/Library/Arrayable.php
@@ -6,7 +6,8 @@
 namespace Fw\Library;
 
 /**
- * Interface Arrayable
+ * Arrayable is the interface that should be implemented by classes
+ * who want to support customizable representation of their instances.
  */
 interface Arrayable
 {
