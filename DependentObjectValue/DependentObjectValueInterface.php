78be6f612ade5a555deefb513d8a5a117bb3d1ff
e.tolubarya <e.tolubarya@908.dp.ua>
2014-12-21 11:16:40 +0200

    Update DependentObjectValueInterface.php

--- DependentObjectValue/DependentObjectValueInterface.php
+++ DependentObjectValue/DependentObjectValueInterface.php
@@ -2,8 +2,8 @@
 
 interface DependentObjectValueInterface
 {
-    public function __construct($value, Closure $callback);
+    public function __construct($value = null, Closure $callback = null);
     public function __toString();
-    public function set($value, Closure $callback);
+    public function set($value = null, Closure $callback = null);
     public function get();
 }
